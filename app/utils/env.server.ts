56b35c23d35b93898849ddedcf3ac616a5ef1375
izaack55182 <l20te0298@tepeaca.tecnm.mx>
2025-06-11 11:15:38 -0600

    Fix Docker File

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -20,6 +20,9 @@ const envSchema = z.object({
 	// POSTHOG
 	POSTHOG_API_KEY: z.string(),
 	POSTHOG_API_ENDPOINT: z.string(),
+
+	// RESEND
+    RESEND_API_KEY: z.string(),
 })
 
 type ServerEnv = z.infer<typeof envSchema>
